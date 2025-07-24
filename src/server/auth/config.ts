a5bde3ff933907961f0af18803517fa30288c0c8
a-kashif-ahmed <kashifmsd7@gmail.com>
2025-07-24 18:03:46 +0530

    Crucial

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -6,7 +6,7 @@ import { db } from "@/server/db";
 import { env } from "@/env";
 
 /**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * Module augmentation for next-auth types. Allows us to add custom properties to the session
  * object and keep type safety.
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
@@ -43,7 +43,7 @@ export const authConfig = {
      * ...add more providers here.
      *
      * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+     * GitHub provider requires you to add the refresh_token_expires_in field to the Account
      * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
      *
      * @see https://next-auth.js.org/providers/github
@@ -61,4 +61,4 @@ export const authConfig = {
       },
     }),
   },
-} satisfies NextAuthConfig;
+} satisfies NextAuthConfig; 
